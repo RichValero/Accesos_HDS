6fd2a0aaa8e6c737ab19246c5eb90f2799d845d4
Richard Valero <richardvalero1106@gmail.com>
2024-09-12 13:49:44 -0300

    commit changes

--- src/components/GridCard.jsx
+++ src/components/GridCard.jsx
@@ -3,32 +3,61 @@ import { cardList } from "../constants";
 import { useState } from "react";
 
 const GridCard = () => {
+  const [filter, setFilter] = useState("all");
+
+  const handleFilter = (category) => {
+    setFilter(category);
+  };
+
+  const filteredCards = cardList.filter(
+    (card) => filter === "all" || card.category === filter
+  );
+
   return (
     <>
+      <div className="flex w-full px-[250px] gap-4 justify-start items-center">
+        <p className="text-primary font-semibold">Filtrar por:</p>
+        <button
+          className="text-primary hover:bg-primary hover:text-white px-4 py-2 rounded-lg font-bold"
+          onClick={() => handleFilter("admin")}
+        >
+          Administrativas
+        </button>
+        <button
+          className="text-primary hover:bg-primary hover:text-white px-4 py-2 rounded-lg font-bold"
+          onClick={() => handleFilter("clinic")}
+        >
+          Clinicas
+        </button>
+        <button
+          className="text-primary hover:bg-primary hover:text-white px-4 py-2 rounded-lg font-bold"
+          onClick={() => handleFilter("all")}
+        >
+          Todas
+        </button>
+      </div>
       <div className="grid grid-cols-6 gap-4 xsm:max-2xl:grid-cols-1 sm:max-2xl:grid-cols-2 md:max-2xl:grid-cols-3 lg:max-2xl:grid-cols-4 px-[250px] sm:max-xl:px-[50px] md:max-xl:px-[100px] py-[100px] ">
-        {cardList.map((card) => (
-          <>
-            <a
-              key={card}
-              href={card.route}
-              className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-primary hover:ring-primary w-[190px] h-[200px]"
-            >
-              <div className="flex justify-center space-x-3">
-                <img
-                  src={card.imgUrl}
-                  width={60}
-                  height={60}
-                  className="rounded-lg"
-                />
-                {/* <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">
+        {filteredCards.map((card) => (
+          <a
+            key={card}
+            href={card.route}
+            className="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-primary hover:ring-primary w-[190px] h-[200px]"
+          >
+            <div className="flex justify-center space-x-3">
+              <img
+                src={card.imgUrl}
+                width={60}
+                height={60}
+                className="rounded-lg"
+              />
+              {/* <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">
                 {cardList.label}
                 </h3> */}
-              </div>
-              <p className="text-primary group-hover:text-white text-2xl text-center font-bold">
-                {card.label}
-              </p>
-            </a>
-          </>
+            </div>
+            <p className="text-primary group-hover:text-white text-2xl text-center font-bold">
+              {card.label}
+            </p>
+          </a>
         ))}
       </div>
     </>
--- src/constants/index.js
+++ src/constants/index.js
@@ -24,18 +24,21 @@ export const cardList = [
     imgUrl: "./assets/icons/intranet.png",
     router: "http://10.6.201.3/MRIS/Login?ReturnUrl=%2fMRIS%2f", //nuevo link mris.hsalvador.cl
     label: "MRIS",
+    category: "clinic",
   },
   {
     id: 3,
     imgUrl: "./assets/icons/syslab.jpg",
     route: "http://10.6.198.50:8080/syslab/index.php",
     label: "SYSLAB",
+    category: "clinic",
   },
   {
     id: 4,
     imgUrl: "./assets/icons/proactive.jpg",
     route: "https://examenesusuarios.hsalvador.cl/",
     label: "Proactive LIS",
+    category: "clinic",
   },
   {
     id: 5,
@@ -43,6 +46,7 @@ export const cardList = [
     route:
       "https://trakcare.intersystemschile.cl/sdor/csp/system.Home.cls#/Component/SSUserLogon",
     label: "Trakcare",
+    category: "clinic",
   },
   {
     id: 6,
@@ -50,72 +54,84 @@ export const cardList = [
     route:
       "https://salud.intersystemschile.cl/sdor/csp/system.Home.cls#/Component/SSUserLogon",
     label: "Trakcare Contingencia",
+    category: "clinic",
   },
   {
     id: 7,
     imgUrl: "./assets/icons/intranet.png",
     route: "http://sam.hsalvador.cl/Loginv2.aspx?ReturnUrl=%2f",
     label: "SAM",
+    category: "clinic",
   },
   {
     id: 8,
     imgUrl: "./assets/icons/intranet.png",
     route: "https://gesdoc.hsalvador.cl/GestorDocumental/login",
     label: "GESDOC",
+    category: "admin",
   },
   {
     id: 9,
     imgUrl: "./assets/icons/lm.jpg",
     route: "https://www.licencia.cl/v2/produccion/",
     label: "Licencias",
+    category: "clinic",
   },
   {
     id: 10,
-    imgUrl: "./assets/intranet.jpg",
+    imgUrl: "./assets/icons/seis.jpg",
     route: "https://salvador.experthis.cl/produccion/login.php",
     label: "SEIS",
+    caterogy: "admin",
   },
   {
     id: 11,
     imgUrl: "./assets/icons/rflex.jpg",
     route: "https://hsalvador.rflex.io/login",
     label: "rFlex",
+    category: "admin",
   },
   {
     id: 12,
     imgUrl: "./assets/icons/serq.jpg",
     route: "https://salud.serq.cl/Account/Index?ReturnUrl=%2f",
     label: "SerQ",
+    category: "admin",
   },
   {
     id: 13,
     imgUrl: "./assets/icons/intranet.png",
     route: "http://10.8.97.136/autoconsulta/",
     label: "Autoconsulta",
+    category: "admin",
   },
   {
     id: 14,
     imgUrl: "./assets/icons/intranet.png",
     route: "http://uap.hsalvador.cl/Login.aspx?ReturnUrl=%2f",
     label: "UAP",
+    category: "clinic",
   },
   {
     id: 15,
     imgUrl: "./assets/icons/intranet.png",
     route: "https://asclepio.hsalvador.cl/",
     label: "Asclepio",
+    category: "clinic",
   },
   {
     id: 16,
     imgUrl: "./assets/icons/intranet.png",
     route: "https://infectologia.hsalvador.cl/",
     label: "Infectologia",
+    category: "clinic",
   },
   {
     id: 17,
     imgUrl: "./assets/icons/intranet.png",
     route: "https://oftalmologia.hsalvador.cl/",
     label: "Oftalmologia",
+    category: "clinic",
   },
   {
     //LINK DE HEMATOLOGIA CAMBIARA.
@@ -124,5 +140,6 @@ export const cardList = [
     route:
       "https://laboratorio.hsalvador.cl/hematologia/Account/Login.aspx?ReturnUrl=%2fhematologia%2fBandejaEntradaMed%2fBandejaEntradaMed.aspx",
     label: "Laboratorio Hematologia",
+    category: "clinic",
   },
 ];
